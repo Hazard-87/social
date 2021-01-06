f0ca28fdd0777f8f779a177e4a4dd9a955e871aa
Hazard-87 <dpetryshkin@gmail.com>
2021-01-06 09:39:15 +0300

    Updates

---
+++ precache-manifest.cee182f058abb333f20f0e805e73d431.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba063f1631bdb3c4e05b3d0fcbc992e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f9b33a778e8950a34e",
+    "url": "/static/css/3.9fef6a1d.chunk.css"
+  },
+  {
+    "revision": "5e2e51a35fa3444b6b46",
+    "url": "/static/css/4.b285054d.chunk.css"
+  },
+  {
+    "revision": "6d87936ff206b295e4dd",
+    "url": "/static/css/5.5fb445dd.chunk.css"
+  },
+  {
+    "revision": "46a7f51a8231ba153499",
+    "url": "/static/css/main.2ccbabd2.chunk.css"
+  },
+  {
+    "revision": "84419f82cf28074ba151",
+    "url": "/static/js/2.44bb7205.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.44bb7205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f9b33a778e8950a34e",
+    "url": "/static/js/3.2a2929f3.chunk.js"
+  },
+  {
+    "revision": "5e2e51a35fa3444b6b46",
+    "url": "/static/js/4.c02b8ae3.chunk.js"
+  },
+  {
+    "revision": "6d87936ff206b295e4dd",
+    "url": "/static/js/5.90dc2e4d.chunk.js"
+  },
+  {
+    "revision": "46a7f51a8231ba153499",
+    "url": "/static/js/main.d1b70f18.chunk.js"
+  },
+  {
+    "revision": "135cf3c6158499134384",
+    "url": "/static/js/runtime-main.78d46533.js"
+  },
+  {
+    "revision": "1d2550da22213f923111153b853d4c22",
+    "url": "/static/media/Preloader.1d2550da.svg"
+  }
+]);
